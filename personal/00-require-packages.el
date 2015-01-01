0275aa7efc2296885957f9e2a31253e0c8e4a9a5
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-02 01:34:57 +0900

    Add some packages.

--- personal/00-require-packages.el
+++ personal/00-require-packages.el
@@ -1,3 +1,16 @@
 (prelude-require-package 'use-package)
 (prelude-require-package 'init-loader)
 (prelude-require-package 'key-chord)
+(prelude-require-package 'smart-newline)
+(prelude-require-package 'twittering-mode)
+(prelude-require-package 'anything)
+(prelude-require-package 'helm-descbinds)
+(prelude-require-package 'viewer)
+(prelude-require-package 'vline)
+(prelude-require-package 'crosshairs)
+(prelude-require-package 'smart-mode-line)
+(prelude-require-package 'web-mode)
+
+
+
+
