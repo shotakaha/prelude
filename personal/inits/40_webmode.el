c1c4bab55c9012ee93ef27df553d20170af9593d
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-02 01:54:32 +0900

    Replaced amode-list using use-package:mode.
             Also delete duplicated file.

--- personal/inits/40_webmode.el
+++ personal/inits/40_webmode.el
@@ -2,16 +2,18 @@
 ;;; web-mode.el
 
 (use-package web-mode
+  :mode ( ("\\.phtml\\'" . web-mode)
+          ("\\.tpl\\.php\\'" . web-mode)
+          ("\\.[gj]sp\\'" . web-mode)
+          ("\\.as[cp]x\\'" . web-mode)
+          ("\\.erb\\'" . web-mode)
+          ("\\.mustache\\'" . web-mode)
+          ("\\.djhtml\\'" . web-mode)
+          ("\\.html?\\'" . web-mode)
+          )
   :config
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (setq web-mode-engines-alist
         '(("php"    . "\\.phtml\\'")
           ("blade"  . "\\.blade\\.")))
-)
+  )
+
