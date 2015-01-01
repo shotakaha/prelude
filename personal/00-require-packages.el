85d8616a43c8f2d6e64fa8189b5dfd5e338c2b3c
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-02 03:28:26 +0900

    Edited

--- personal/00-require-packages.el
+++ personal/00-require-packages.el
@@ -13,7 +13,14 @@
 (prelude-require-package 'auto-complete)
 (prelude-require-package 'ac-ispell)
 
-(prelude-require-package 'guide-key))
+(prelude-require-package 'guide-key)
+(prelude-require-package 'migemo)
+(prelude-require-package 'helm-migemo)
+(prelude-require-package 'helm-swoop)
+(prelude-require-package 'ace-isearch)
+
+
+
 
 
 
--- personal/inits/48_google.el
+++ personal/inits/48_google.el
@@ -32,12 +32,46 @@
 ;;; google-translage.el ---
 ;;; Emacs interface to Google Translate.
 ;;; UIが違うらしいのでどちらかえらぶ
+;;; http://rubikitch.com/2014/12/07/google-translate/
+;;; http://rubikitch.com/2014/12/30/google-translate-sentence/
 
 ;;(require 'google-translate)
 ;; (require 'google-translate-default-ui)
 ;; (global-set-key (kbd "C-c t") 'google-translate-at-point)
 ;; (global-set-key (kbd "C-c T") 'google-translate-query-translate)
 
-(use-package google-translate)
-(use-package google-translate-smooth-ui)
-(global-set-key (kbd "C-c t") 'google-translate-smooth-translate)
+
+(use-package google-translate
+  :config
+  (defvar google-translate-english-chars "[:ascii:]"
+    "これらの文字が含まれているときは英語とみなす")
+  (defun google-translate-enja-or-jaen (&optional string)
+    "regionか、現在のセンテンスを言語自動判別でGoogle翻訳する。"
+    (interactive)
+    (setq string
+          (cond ((stringp string) string)
+                (current-prefix-arg
+                 (read-string "Google Translate: "))
+                ((use-region-p)
+                 (buffer-substring (region-beginning) (region-end)))
+                (t
+                 (save-excursion
+                   (let (s)
+                     (forward-char 1)
+                     (backward-sentence)
+                     (setq s (point))
+                     (forward-sentence)
+                     (buffer-substring s (point)))))))
+    (let* ((asciip (string-match
+                    (format "\\`[%s]+\\'" google-translate-english-chars)
+                    string)))
+      (run-at-time 0.1 nil 'deactivate-mark)
+      (google-translate-translate
+       (if asciip "en" "ja")
+       (if asciip "ja" "en")
+       string)))
+  (global-set-key (kbd "C-c t") 'google-translate-enja-or-jaen)
+  )
+
+;; (use-package google-translate-smooth-ui)
+;; (global-set-key (kbd "C-c t") 'google-translate-smooth-translate)
