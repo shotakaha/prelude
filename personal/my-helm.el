48079fe8946f6856e4d60c7cb9b3b24ec13315b4
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-05 17:55:01 +0900

    Moved helm-migemo config to my-helm.el

--- personal/my-helm.el
+++ personal/my-helm.el
@@ -8,6 +8,24 @@
 ;;; http://rubikitch.com/2014/12/19/helm-migemo/
 (prelude-require-package 'helm-migemo)
 
+(use-package helm-migemo
+  :disabled t
+  :config
+  ;; この修正が必要
+  (eval-after-load "helm-migemo"
+    '(defun helm-compile-source--candidates-in-buffer (source)
+       (helm-aif (assoc 'candidates-in-buffer source)
+           (append source
+                   `((candidates
+                      . ,(or (cdr it)
+                             (lambda ()
+                               ;; Do not use `source' because other plugins
+                               ;; (such as helm-migemo) may change it
+                               (helm-candidates-in-buffer (helm-get-current-source)))))
+                     (volatile) (match identity)))
+         source)))
+  )
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; migemo.el
 ;;; http://rubikitch.com/2014/08/20/migemo/
