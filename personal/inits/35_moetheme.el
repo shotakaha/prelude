fb118975f4856cb06680ee944a4932c9d8914af6
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-05 17:38:03 +0900

    Move uniquify config to init.el.
    	deleted:    personal/inits/25_uniquify.el
    	deleted:    personal/inits/35_moetheme.el

--- personal/inits/35_moetheme.el
+++
@@ -1,9 +0,0 @@
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;; moe-theme.el   目に辛かったので delete した
-;; (require 'moe-theme)
-;; ;;; org-mode で 見出し毎にフォントの大きさを変える
-;; (setq moe-theme-resize-org-title
-;;       '(2.2 1.8 1.6 1.4 1.2 1.0 1.0 1.0 1.0))
-;; ;;; mode-line をオレンジ色にする
-;; (setq moe-theme-mode-line-color 'orange)
-;; (moe-light)
