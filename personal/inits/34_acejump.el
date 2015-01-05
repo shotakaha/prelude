61da984df794acf5ce29641dda4932af2e5dbded
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-05 18:01:56 +0900

    Move ace-isearch config to my-helm.el
    Remove extra prelude-require-package from 00-.

--- personal/inits/34_acejump.el
+++ personal/inits/34_acejump.el
@@ -1,10 +1,3 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; ace-jump-buffer.el
 (use-package ace-jump-buffer)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; ace-isearch.el
-(use-package ace-isearch
-  :config
-  (global-ace-isearch-mode 1)
-)
