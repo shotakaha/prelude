64af9ce666df1c5109a76b71ab62b0de64793d82
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-06 18:30:26 +0900

    Modified view-mode related stuff.

--- personal/my-view.el
+++ personal/my-view.el
@@ -1,29 +1,60 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; view-mode
-(use-package view)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Enable view-mode
+;;; るびきち : Emacsテクニックバイグル : p.217
+;;; るびきち : Emacsテクニックバイグル : p.218
 ;; Open with 'C-x C-r'
 ;; Toggle with 'C-x C-q'
+;; Enable view-mode
 (setq view-read-only t)
+(use-package view
+  ;; less-like
+  (define-key view-mode-map (kbd "N") 'View-search-last-regexp-backward)
+  (define-key view-mode-map (kbd "?") 'View-search-regexp-backward)
+  (define-key view-mode-map (kbd "G") 'View-goto-line)
+  (define-key view-mode-map (kbd "b") 'View-scroll-page-backward)
+  (define-key view-mode-map (kbd "f") 'View-scroll-page-forward)
+  ;;vi/w3m-like
+  (define-key view-mode-map (kbd "h") 'backward-char)
+  (define-key view-mode-map (kbd "j") 'next-line)
+  (define-key view-mode-map (kbd "k") 'previous-line)
+  (define-key view-mode-map (kbd "l") 'forward-char)
+  (define-key view-mode-map (kbd "J") 'View-scroll-line-forward)
+  (define-key view-mode-map (kbd "J") 'View-scroll-line-backward)
+  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; viewer.el --- improve view-mode
+;;; るびきち : Emacsテクニックバイグル : p.220
+;;; p.220 - view-mode の mode-line に色を付ける
+;;; p.221 - メジャーモードに合わせて view-mode のキーバインドを設定する
+;;; p.222 - 特定のファイルを view-mode で開く
+
 (use-package viewer
   :config
   (viewer-stay-in-setup)
-;;; set color to mode-line
+  ;; set color to mode-line
+  ;; 書き込み不可の場合 : tomato
   (setq viewer-modeline-color-unwritable "tomato")
+  ;; view-mode の場合 : orange
   (setq viewer-modeline-color-view "orange")
   (viewer-change-modeline-color-setup)
-;;; set view-mode kbd according to major-mode
+
+  ;; set view-mode kbd according to major-mode
+  ;; カーソル位置の関数定義にジャンプするコマンドをRETに統一
   (define-overriding-view-mode-map c-mode
     ("RET" . gtags-find-tag-from-here))
   (define-overriding-view-mode-map emacs-lisp-mode
     ("RET" . find-function-at-point))
-;;; open specific file in view-mode
-  (setq view-mode-by-default-regexp "\\.log$")
-;;;(viewer-aggressive-setup 'force)    ;;; very very aggressive
-)
 
+  ;; ログファイル（.log）は view-mode 開く
+  (setq view-mode-by-default-regexp "\\.log$")
+  ;; すべてファイルを view-mode で開く
+  ;; ただし、文字数の少ないファイルは除く（デフォルト100文字以下）
+  ;; (viewer-aggressive-minimum-size 100)
+  ;; (viewer-aggressive-setup 'force)
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; origami.el
+;;; http://rubikitch.com/2015/01/03/origami/
