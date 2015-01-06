b510f525d97a0141e8ae1dae98a3ce3f89e9bee9
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-06 21:00:35 +0900

    Add origami config.

--- personal/my-view.el
+++ personal/my-view.el
@@ -58,3 +58,27 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; origami.el
 ;;; http://rubikitch.com/2015/01/03/origami/
+;;; http://rubikitch.com/2015/01/05/origami-2/
+(prelude-require-package 'origami)
+(use-package origami
+  :ensure t
+  :config
+  ;; (makunbound 'origami-view-mode-map)
+  (define-minor-mode origami-view-mode
+    "TABにorigamiの折畳みを割り当てる"
+    nil "折紙"
+    '(("\C-i" . origami-cycle))
+    (or origami-mode (origami-mode 1)))
+
+  (defun origami-cycle (recursive)
+    "origamiの機能をorg風にまとめる"
+    (interactive "P")
+    (call-interactively
+     (if recursive 'origami-toggle-all-nodes 'origami-toggle-node)))
+
+  (defun view-mode-hook--origami ()
+    (when (memq major-mode (mapcar 'car origami-parser-alist))
+      (origami-view-mode (if view-mode 1 -1))))
+
+  (add-hook 'view-mode-hook 'view-mode-hook--origami)
+  )
