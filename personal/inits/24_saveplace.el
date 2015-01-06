ab8c27d5277e1e9a1578de4b7d769adebaab2fcd
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-06 22:30:34 +0900

    Delete file.
        deleted:    personal/inits/24_saveplace.el
    
        saveplace already configured in core/prelude-editor.el

--- personal/inits/24_saveplace.el
+++
@@ -1,5 +0,0 @@
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;; saveplace.el
-;; (require 'saveplace)
-;; (setq-default save-place t)
-;; (setq save-place-file "~/.emacs.d/saved-places")
