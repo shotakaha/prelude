3a84af10be2b354e34f8862b1f68b90b8ab8ca79
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-07 17:43:12 +0900

    Edit keybinds for view-mode.
         Replaced to bind-keys.
         Add u and d (half-scroll).

--- personal/my-view.el
+++ personal/my-view.el
@@ -8,19 +8,23 @@
 (setq view-read-only t)
 (use-package view
   :config
-  ;; less-like
-  (define-key view-mode-map (kbd "N") 'View-search-last-regexp-backward)
-  (define-key view-mode-map (kbd "?") 'View-search-regexp-backward)
-  (define-key view-mode-map (kbd "G") 'View-goto-line)
-  (define-key view-mode-map (kbd "b") 'View-scroll-page-backward)
-  (define-key view-mode-map (kbd "f") 'View-scroll-page-forward)
-  ;;vi/w3m-like
-  (define-key view-mode-map (kbd "h") 'backward-char)
-  (define-key view-mode-map (kbd "j") 'next-line)
-  (define-key view-mode-map (kbd "k") 'previous-line)
-  (define-key view-mode-map (kbd "l") 'forward-char)
-  (define-key view-mode-map (kbd "J") 'View-scroll-line-forward)
-  (define-key view-mode-map (kbd "J") 'View-scroll-line-backward)
+  (bind-keys :map view-mode-map
+             ;; less-like
+             ("N" . View-search-last-regexp-backward)
+             ("?" . View-search-regexp-backward)
+             ("G" . View-goto-line)
+             ("b" . View-scroll-page-backward)
+             ("f" . View-scroll-page-forward)
+             ("d" . View-scroll-half-page-forward)
+             ("u" . View-scroll-half-page-backward)
+             ;;vi/w3m-like
+             ("h" . backward-char)
+             ("j" . next-line)
+             ("k" . previous-line)
+             ("l" . forward-char)
+             ("J" . View-scroll-line-forward)
+             ("K" . View-scroll-line-backward)
+             )
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
