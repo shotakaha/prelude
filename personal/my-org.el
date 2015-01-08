e61128aafdf19427143decdefaa93836f61349ff
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-08 20:34:08 +0900

    Edit org-capture config.
         Set org-capture-templates.
         Keybinded to C-c c.

--- personal/my-org.el
+++ personal/my-org.el
@@ -21,10 +21,11 @@
 
   ;; 保存先（もっとうまく書けたらいいのになぁ）
   (setq my-org-directory "~/Documents/org/")
-  (setq my-org-agenda-directory "~/Documents/org/agenda/")
-  (setq my-org-capture-file "~/Documents/org/captured-demo.org")
+  (setq my-org-capture-demo-file "~/Documents/org/captured-demo.org")
   (setq my-org-codereading-file "~/Documents/org/codereading.org")
+  (setq my-org-agenda-directory "~/Documents/org/agenda/")
 
+  ;; 基本設定
   ;; 見出しの余分な * を消す。意外と見やすくなる。
   (setq org-hide-leading-stars t)
   ;; これはなんだろう？
@@ -36,49 +37,57 @@
   ;; デフォルトのファイル名
   (setq org-default-notes-file "captured.org")
 
-  ;; TODOの設定 --- tamura70ブログ
-  ;; http://d.hatena.ne.jp/tamura70/20100207/org
-  ;; http://d.hatena.ne.jp/tamura70/20100215/org
-  ;; TODOの状態の設定
-  ;; ! をつけることで、その状態へ変更した日時を記録することが可能
-  ;; @ をつけることで、その状態へ変更した時にメモを残すことが可能
-  (setq org-todo-keywords
-        '((sequence "APPT(a)" "TODO(t)" "STARTED(s!)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCEL(c@/!)")))
-  (setq org-log-done 'time)   ;;; DONEの時刻を記録
-  ;; (setq org-log-done 'note)  ;;; DONEの時刻とメモを記録
-
-  ;; TAGの設定 --- tamura70のブログ
-  ;; http://d.hatena.ne.jp/tamura70/20100215/org
-  ;; TAGリストの一括設定
-  ;; orgファイル毎で設定する場合は，ファイル中に以下のように記述する．
-  ;; #+TAGS: @OFFICE(o) @HOME(h)
-  ;; #+TAGS: SHOPPING(s) MAIL(m) PROJECT(p)
-  (setq org-tag-alist
-        '(("@OFFICE" . ?o) ("@HOME" . ?h)
-          ("MAIL" . ?m) ("WRITE" . ?w)
-          ("ASK" . ?a)))
-
-  ;; ;; AGENDAの設定 --- tamura70ブログ
-  ;; ;; http://d.hatena.ne.jp/tamura70/20100208/org
-  ;; ;; アジェンダ表示の対象ファイル
-  (setq org-agenda-files (list my-org-agenda-directory))
-  ;; アジェンダ表示で下線を用いる
-  (add-hook 'org-agenda-mode-hook '(lambda () (hl-line-mode 1)))
-  ;; (setq hl-line-face 'underline)
-  ;; 標準の祝日を利用しない
-  (setq calendar-holidays nil)
-
   ;; org-capture の設定
-  ;; http://d.hatena.ne.jp/rubikitch/20090121/1232468026
-  ;; まずはデモを試してみる
-  ;; C-x C-z に割り当ててある
-  ;; 保存先は my-org-capture-file にした
-  ;; http://d.hatena.ne.jp/rubikitch/20100819/org
-  ;; setq org-capture-templates の部分は外に抜き出した。
+  ;; 1. org-capture-demo
+  ;;    http://d.hatena.ne.jp/rubikitch/20100819/org
+  ;;    demoを作成し、改変していく
+  ;;    保存先は my-org-capture-demo-file にする
+  ;; 2. org-capture-code-reading
+  ;;    http://d.hatena.ne.jp/rubikitch/20090121/1232468026
+  ;;    org-remember -> org-capture に置き換える
+
+  ;; demoをイジったり、しらべたりしてわかったこと:
+  ;;
+
+  ;; org-capture-templates の引数 => org-capture.el を読むのが一番
+  ;; <file:~/.emacs.d/elpa/org-20150105/org-capture.el>
+  ;; 1. key
+  ;;    ショートカットキー
+  ;; 2. description
+  ;;    org-capture したときに表示されるメッセージ
+  ;; 3. type {entry, item, checkitem, table-line, plain}
+  ;;    入力内容の種類
+  ;;    とりあえず、entryにしとけばよい
+  ;; 4. target : default = org-default-notes-file
+  ;;    - 使いそうなものを挙げておく
+  ;;      - (file "path-to-file")
+  ;;      - (id "id-of-existing-entry")
+  ;;      - (file+headline "path-to-file" "node-headline")
+  ;;      - (file+datetree "path-to-file")
+  ;;      - (file+datetree+prompt "path-to-file")
+  ;;      - (clock)
+  ;; 5. template
+  ;;    - org-capture した時に自動で挿入される文章
+  ;;      - (file "path-to-template-file")
+  ;;      - テンプレートで使えるエスケープ文字
+  ;;      - %t, %T, %u, %U = 日時（日付のみ、日付＋時刻）（inactive な日時）
+  ;;      - %a = Annotation (default:org-store-link)
+  ;;      - %n = username (user-full-name)
+  ;;      - %f, %F = org-capture した場所のファイル名（フルパス）
+  ;;      - %:keyword = 便利なテンプレがありそうだが、使い方がよくわからない
+  ;;      - %? = テンプレ挿入後のカーソルの位置
+  ;; 6以降 : property
+  ;;    - テンプレートの挿入位置、など
+  ;;      - :prepend
+  ;;      - :immediate-finish = テンプレートをちゃんと用意しておけば、ファイルのブックマークを作れそう
+  ;;      - :jump-to-captured = 編集後、capture したファイルを開く
+  ;;      - :empty-lines, empty-lines-before, :empty-lines-after = エントリーの前後にいれる空行の数を指定する
+  ;;      - :clock-in, clock-keep, clock-resume = org-capture したときに、clockをどうするか
+  ;;      - :unnarrowed = org-capture した時に全表示する（デフォルトは、追記部分のみ表示）
 
   (defun org-capture-demo ()
     (interactive)
-    (let ((file my-org-capture-file)
+    (let ((file my-org-capture-demo-file)
           org-capture-templates)
       (find-file-other-window file)
       (unless (save-excursion
@@ -87,33 +96,53 @@
         (insert "#+title: org-capture-demo\n* entry\n"))
       (other-window 1)
 
-      (setq org-capture-templates
-            `(("a" "ふつうのエントリー後に追加" entry (file+headline ,file "entry") "* %?\n%U\n%a\n")
-              ("b" "ふつうのエントリー前に追加" entry (file+headline ,file "entry") "* %?\n%U\n%a\n" :prepend t)
-              ("c" "即座に書き込み" entry (file+headline ,file "entry") "* immediate-finish\n" :immediate-finish t)
-              ("d" "ナローイングしない" entry (file+headline ,file "entry") "* 全体を見る\n\n" :unnarrowed t)
-              ("e" "クロック中のエントリに追加" entry (clock) "* clocking" :unnarrowed t)
-              ("f" "リスト" item (file+headline ,file "list") "- リスト")
-              ;; うまく動かない
-              ("g" "チェックリスト" checkitem (file+headline ,file "list") "チェックリスト")
-              ("h" "表の行" table-line (file+headline ,file "table") "|表|")
-              ("i" "そのまま" plain (file+headline ,file "plain") "あいうえお")
-              ("j" "ノードをフルパス指定して挿入" entry (file+olp ,file "test" "entry") "* %?\n%U\n%a\n")
-              ("k" "ノードを正規表現指定して挿入" entry (file+regexp ,file "list") "* %?\n%U\n%a\n")
-              ;; 年月日エントリは追記される
-              ("l" "年/月/日のエントリを作成する1" entry (file+datetree ,file))
-              ("m" "年/月/日のエントリを作成する2" item  (file+datetree ,file))
-              ("o" "年/月/日のエントリを作成する prepend" entry (file+datetree ,file) "* a" :prepend t))
-            )
+      ;; (setq org-capture-templates
+      ;;       `(("a" "ふつうのエントリー後に追加" entry (file+headline ,file "entry") "* %?\n%U\n%a\n%f\n")
+      ;;         ("b" "ふつうのエントリー前に追加" entry (file+headline ,file "entry") "* %?\n%U\n%a\n" :prepend t)
+      ;;         ("c" "即座に書き込み" entry (file+headline ,file "entry") "* immediate-finish\n" :immediate-finish t)
+      ;;         ("d" "ナローイングしない" entry (file+headline ,file "entry") "* 全体を見る\n\n" :unnarrowed t)
+      ;;         ("e" "クロック中のエントリに追加" entry (clock) "* clocking" :unnarrowed t)
+      ;;         ("f" "リスト" item (file+headline ,file "list") "- リスト")
+      ;;         ;; うまく動かない
+      ;;         ("g" "チェックリスト" checkitem (file+headline ,file "list") "チェックリスト")
+      ;;         ("h" "表の行" table-line (file+headline ,file "table") "|表|")
+      ;;         ("i" "そのまま" plain (file+headline ,file "plain") "あいうえお")
+      ;;         ("j" "ノードをフルパス指定して挿入" entry (file+olp ,file "test" "entry") "* %?\n%U\n%a\n")
+      ;;         ("k" "ノードを正規表現指定して挿入" entry (file+regexp ,file "list") "* %?\n%U\n%a\n")
+      ;;         ;; 年月日エントリは追記される
+      ;;         ("l" "年/月/日のエントリを作成する1" entry (file+datetree ,file))
+      ;;         ("m" "年/月/日のエントリを作成する2" item  (file+datetree ,file))
+      ;;         ("o" "年/月/日のエントリを作成する prepend" entry (file+datetree ,file) "* a" :prepend t))
+      ;;       )
       (org-capture)
       ))
+
+  (setq org-capture-templates
+        `(("a" "とっさのメモなど" entry
+           (file+headline nil "MEMO")
+           "* %U%?\n\na:%a\nf:%f\nF:%F\n"
+           :empty-lines 1)
+
+          ("m" "会議の議事録など" entry
+           (file+datetree "~/Documents/org/minutes.org")
+           "* %? %T"
+           :empty-lines 1
+           :jump-to-captured 1)
+
+          ("p" "ぷれーん" plain
+           (file+headline nil "plain")
+           "%?"
+           :empty-lines 1)
+          )
+        )
+
 
   ;; ;; org-capture-templates 変数の設定
   ;; (setq org-capture-templates
   ;;       '(("t" "Todo" entry (file+headline nil "Inbox")     "** TODO %?\n    %i\n    %a\n    %t")
   ;;         ("b" "Bug"  entry (file+headline nil "Inbox")     "** TODO %?    :bug:\n    %i\n    %a\n    %t")
   ;;         ("i" "Idea" entry (file+headline nil "New Ideas") "** %?\n    %i\n    %a\n    %t")
-  ;;         ("l" "Log"  entry (file+headline nil "Log")       "** %?\n    %i\n    %a\n    %t")
+  ;;
   ;;         ("d" "Dict" entry (file+headline nil "Dict")      "** %?\n    %i\n    %a\n    %t")
   ;;         )
   ;;       '(("Note" ?n "** %?\n    %i\n    %a\n    %t"       nil "Inbox")
@@ -155,72 +184,42 @@
 
   (bind-key "C-x C-z" 'org-capture-demo)
   ;; (bind-key "C-x C-x" 'org-capture-code-reading)
+  (bind-key "C-c c" 'org-capture)
 
 
 
-  ;; ひき続きるびきちブログに書いてあった、リンク間移動の設定
-  ;; るびきちブログによると
-  ;; 『「C-c C-x C-p」と「C-c C-x C-n」*2を押すとブラウザで
-  ;;   Tabを押すようにリンクからリンクの移動ができるのだが、
-  ;;   そのままだと隠れているリンクに到達してしまうのでいまいち不便だ。
-  ;;   見えている部分のみのリンク間移動のコマンドを定義する。
-  ;;   「M-p」と「M-n」に割り当てておいた。
-  ;;   これで検索結果から快適にリンク先に飛べるぞ』
-  ;; 正直、あまりorgを使いこなせていないので、
-  ;; リンク間移動をしたことないんだけど、便利そうなので設定しておく
+  ;; TODOの設定 --- tamura70ブログ
+  ;; http://d.hatena.ne.jp/tamura70/20100207/org
+  ;; http://d.hatena.ne.jp/tamura70/20100215/org
+  ;; TODOの状態の設定
+  ;; ! をつけることで、その状態へ変更した日時を記録することが可能
+  ;; @ をつけることで、その状態へ変更した時にメモを残すことが可能
+  (setq org-todo-keywords
+        '((sequence "APPT(a@/!)" "TODO(t)" "STARTED(s!)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCEL(c@/!)")))
+  (setq org-log-done 'time)   ;;; DONEの時刻を記録
+  ;; (setq org-log-done 'note)  ;;; DONEの時刻とメモを記録
 
-  ;; 次の見えているリンクへ移動
-  (defun org-next-visible-link ()
-    "Move forward to the next link.
-  If the link is in hidden text, expose it."
-    (interactive)
-    (when (and org-link-search-failed (eq this-command last-command))
-      (goto-char (point-min))
-      (message "Link search wrapped back to beginning of buffer"))
-    (setq org-link-search-failed nil)
-    (let* ((pos (point))
-           (ct (org-context))
-           (a (assoc :link ct))
-           srch)
-      (if a (goto-char (nth 2 a)))
-      (while (and (setq srch (re-search-forward org-any-link-re nil t))
-                  (goto-char (match-beginning 0))
-                  (prog1 (not (eq (org-invisible-p) 'org-link))
-                    (goto-char (match-end 0)))))
-      (if srch
-          (goto-char (match-beginning 0))
-        (goto-char pos)
-        (setq org-link-search-failed t)
-        (error "No further link found"))))
+  ;; TAGの設定 --- tamura70のブログ
+  ;; http://d.hatena.ne.jp/tamura70/20100215/org
+  ;; TAGリストの一括設定
+  ;; orgファイル毎で設定する場合は，ファイル中に以下のように記述する．
+  ;; #+TAGS: @OFFICE(o) @HOME(h)
+  ;; #+TAGS: SHOPPING(s) MAIL(m) PROJECT(p)
+  (setq org-tag-alist
+        '(("@OFFICE" . ?o) ("@HOME" . ?h)
+          ("MAIL" . ?m) ("WRITE" . ?w)
+          ("ASK" . ?a)))
 
-  ;; 前の見えているリンクへ移動
-  (defun org-previous-visible-link ()
-    "Move backward to the previous link.
-  If the link is in hidden text, expose it."
-    (interactive)
-    (when (and org-link-search-failed (eq this-command last-command))
-      (goto-char (point-max))
-      (message "Link search wrapped back to end of buffer"))
-    (setq org-link-search-failed nil)
-    (let* ((pos (point))
-           (ct (org-context))
-           (a (assoc :link ct))
-           srch)
-      (if a (goto-char (nth 1 a)))
-      (while (and (setq srch (re-search-backward org-any-link-re nil t))
-                  (goto-char (match-beginning 0))
-                  (not (eq (org-invisible-p) 'org-link))))
-      (if srch
-          (goto-char (match-beginning 0))
-        (goto-char pos)
-        (setq org-link-search-failed t)
-        (error "No further link found"))))
+  ;; ;; AGENDAの設定 --- tamura70ブログ
+  ;; ;; http://d.hatena.ne.jp/tamura70/20100208/org
+  ;; ;; アジェンダ表示の対象ファイル
+  (setq org-agenda-files (list my-org-agenda-directory))
+  ;; アジェンダ表示で下線を用いる
+  (add-hook 'org-agenda-mode-hook '(lambda () (hl-line-mode 1)))
+  ;; (setq hl-line-face 'underline)
+  ;; 標準の祝日を利用しない
+  (setq calendar-holidays nil)
 
-  (bind-keys :map org-mode-map
-             ("M-n" 'org-next-visible-link)
-             ("M-p" 'org-previous-visible-link)
-             )
-  )
 
 
 ;; ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -247,3 +246,4 @@
 ;; ;; ;;       '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(x)" "CANCEL(c)")
 ;; ;; ;;         (sequence "APPT(a)" "|" "DONE(x)" "CANCEL(c)")))
 ;;   )
+  )
