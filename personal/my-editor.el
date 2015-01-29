1032c90b38b507b6987909ec9668a2f70acf065d
Shota TAKAHASHI <shotakaha@gmail.com>
2015-01-29 14:54:12 +0900

    update configuration.
           my-zzz.el の内容をコピー

--- personal/my-editor.el
+++ personal/my-editor.el
@@ -1,3 +1,15 @@
+;;; my-editor.el --- basic Emacs settings
+
+;;; Commentary:
+;; とりあえずなんでも放り込め
+;;
+;; 1. view-modeの設定（less-like, vi-like なキーバインドの追加）
+;; 2. origamiの設定
+;; 3. ido-vertical-modeの設定
+;; 4. psessionの設定
+;; 5. web-modeの設定
+;; 6. jawordの設定
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; view-mode
 ;;; るびきち : Emacsテクニックバイグル : p.217
@@ -89,6 +101,11 @@
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ido - ファイル・バッファを選択して開く
+;;; coreで定義済み
+;; (ido-mode 1)
+;; (ido-everywhere 1)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; ido-vertical-mode.el
 ;;; http://rubikitch.com/2015/01/06/ido-vertical-mode/
 (prelude-require-package 'ido-vertical-mode)
@@ -136,4 +153,108 @@
     )
   (add-hook 'web-mode-hook  'my-web-mode-hook)
 
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; jaword
+;;; http://rubikitch.com/2015/01/27/jaword/
+;;; http://qiita.com/zk_phi/items/b9c90014c434b75e8139
+;;; https://github.com/zk-phi/jaword
+(prelude-require-package 'jaword)
+(use-package jaword
+  :ensure t
+  :config
+  (global-jaword-mode t)
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ファイル・バッファの切替
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; bs : 手軽にバッファ選択
+;;; C-x C-b : helm-buffer-list に設定済み
+;; (global-set-key (kbd "C-x C-b") 'bs-show)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ffap : C-x C-f に機能を追加する
+;;; C-x C-f : helm-find-files に設定済み
+(ffap-bindings)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; recentf-ext : 最近使ったファイルを開く
+;;; recentf は Preludeで定義済み
+;; 最近のファイル500個を保存する
+(setq recentf-max-saved-items 500)
+;; 最近使ったファイルに加えないファイルを
+;; 正規表現で指定する
+(setq recentf-exclude
+      '("/TAGS$" "/var/tmp/")
+      )
+(use-package recentf-ext
+  :ensure t
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; カーソル移動
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; point-undo : カーソルいちを戻す
+(use-package point-undo
+  :ensure t
+  :config
+  (global-set-key [f7] 'point-undo)
+  (global-set-key [M-f7] 'point-redo)
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; goto-chg : 編集箇所の履歴をたどる
+(use-package goto-chg
+  :ensure t
+  :config
+  (global-set-key [f8] 'goto-last-change)
+  (global-set-key [M-f8] 'goto-last-change-reverse)
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; bm : 現在位置をハイライト付きで永続的に記憶させる
+(setq-default bm-buffer-persistence nil)
+(setq bm-restore-repository-on-load t)
+(use-package bm
+  :ensure t
+  :config
+  (add-hook 'find-file-hook 'bm-buffer-restore)
+  (add-hook 'kill-buffer-hook 'bm-buffer-save)
+  (add-hook 'after-save-hook 'bm-buffer-save)
+  (add-hook 'after-revert-hook 'bm-buffer-restore)
+  (add-hook 'vc-before-checkin-hook 'bm-buffer-save)
+  (add-hook 'kill-emacs-hook '(lambda nil (bm-buffer-save-all) (bm-repository-save)))
+
+  (global-set-key (kbd "M-SPC") 'bm-toggle)
+  ;; (global-set-key (kbd "M-") 'bm-previous)
+  ;; (global-set-key (kbd "M-") 'bm-next)
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; imenu : 見出し・関数定義にジャンプする
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 編集
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; visual-regexp : 正規表現置換を対話的に行う
+(use-package visual-regexp
+  :ensure t
+  :config
+  (global-set-key (kbd "M-%") 'vr/query-replace)
+  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; rectangle-mark-mode : 矩形編集
+;;; C-x SPC
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; browse-kill-ring : kill-ringをフル活用
+;;; core/prelude-editor で定義済み
+;;; prelude-key-chord.el でバインド済み
+(use-package browse-kill-ring
+  :ensure t
+  :config
+  (global-set-key (kbd "M-y") 'browse-kill-ring)
   )
