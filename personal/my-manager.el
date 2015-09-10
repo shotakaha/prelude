971a7c793da4639c92a8c850d7db264098d00541
Shota TAKAHASHI <shotakaha@gmail.com>
2015-09-10 10:59:00 +0900

    M : Add comments

--- personal/my-manager.el
+++ personal/my-manager.el
@@ -3,22 +3,32 @@
 ;;; Commentary
 ;; ID管理パッケージの設定
 
+;; GnuPGのインストールが必要
+;; $ sudo port install gnupg
+
 ;; 1. id-manager.el
 ;; 2. passthword.el
 
+(setq epa-file-cache-passphrase-for-symmetric-encryption t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; id-manager.el
 ;;; https://github.com/kiwanami/emacs-id-manager
+;;; Helm式パスワードマネージャ
+;;; コメントを付けれるので、こっちの方がいいのかも
 (use-package id-manager
+  :ensure t
   :config
   (setq idm-database-file "~/.emacs.d/idm-db.gpg")
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; passthword
-(setq epa-file-cache-passphrase-for-symmetric-encryption t)
+;;; Ido式パスワードマネージャ
+;;; Helmを使わないので操作が単純
+(use-package passthword
+  :ensure t
+  )
 
-
-(provide my-manager)
+(provide 'my-manager)
 ;;; my-manager.el ends here
