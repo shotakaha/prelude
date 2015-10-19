b2a5c9582ba29a2c35833219c57f0a1162ff2d0f
Shota TAKAHASHI <shotakaha@gmail.com>
2015-10-19 11:35:36 +0900

    M : Disabeld all commands in 99_keybinds.el

--- personal/inits/99_keybinds.el
+++ personal/inits/99_keybinds.el
@@ -2,29 +2,29 @@
 ;;; Key-bindings
 ;;; use bind-key instead of global-set-key
 ;;; see http://rubikitch.com/2014/09/10/bind-key/ for detail
-(require 'bind-key)
-(bind-key "C-h" 'delete-backward-char)
-(bind-key "M-z" 'goto-line)
-(bind-key "M-ESC ESC" 'keyboard-quit)
-(bind-key "C-x +" 'follow-delete-other-windows-and-split)
+;; (require 'bind-key)
+;; (bind-key "C-h" 'delete-backward-char)
+;; (bind-key "M-z" 'goto-line)
+;; (bind-key "M-ESC ESC" 'keyboard-quit)
+;; (bind-key "C-x +" 'follow-delete-other-windows-and-split)
 ;; (global-set-key (kbd "C-c c") '(lambda() (interactive) (compile "make -k")))
 ;; (global-set-key (kbd "C-c C") '(lambda() (interactive) (compile "make clean")))
 ;; (global-set-key (kbd "C-M-p") '(lambda () (interactive) (other-window -1)))  ;; C-x o
 ;; (global-set-key (kbd "C-M-n") '(lambda () (interactive) (other-window 1)))   ;; C-x o
-(bind-key "C-c C-l" 'toggle-truncate-lines)
-;; (bind-key "C-c C-j" 'open-junk-file)
-(bind-key* "C-c C-m" 'org-capture)
-(bind-key "C-c a" 'org-agenda)
-(bind-key "C-c :" 'ace-jump-buffer)
-(bind-key "C-x :" 'anything-for-files)
-(bind-key "M-y" 'anything-show-kill-ring)
-(bind-key "C-x a s" 'auto-save-buffers-enhanced-toggle-activity)
-(bind-key "M-;" 'comment-dwim-2)
-;; (bind-key "C-x C-f" 'helm-find-files)
-(bind-key [f12] 'eval-buffer)
-(bind-key "C-x C-j" 'direx:jump-to-directory)
-(bind-key "C-\\" 'direx-project:jump-to-project-root-other-window)
-(bind-key "g" 'dired-k dired-mode-map)
+;; (bind-key "C-c C-l" 'toggle-truncate-lines)
+;; ;; (bind-key "C-c C-j" 'open-junk-file)
+;; (bind-key* "C-c C-m" 'org-capture)
+;; (bind-key "C-c a" 'org-agenda)
+;; (bind-key "C-c :" 'ace-jump-buffer)
+;; (bind-key "C-x :" 'anything-for-files)
+;; (bind-key "M-y" 'anything-show-kill-ring)
+;; (bind-key "C-x a s" 'auto-save-buffers-enhanced-toggle-activity)
+;; (bind-key "M-;" 'comment-dwim-2)
+;; ;; (bind-key "C-x C-f" 'helm-find-files)
+;; (bind-key [f12] 'eval-buffer)
+;; (bind-key "C-x C-j" 'direx:jump-to-directory)
+;; (bind-key "C-\\" 'direx-project:jump-to-project-root-other-window)
+;; (bind-key "g" 'dired-k dired-mode-map)
 ;;(bind-key "K" 'direx-k direx:direx-modemap)
 ;; (bind-keys :map dired-mode-map
 ;;            ("o" . dired-omit-mode)
@@ -33,12 +33,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; C-x 3 C-x o --> C-t
 ;;; copied from http://d.hatena.ne.jp/rubikitch/20100210/emacs
-(defun other-window-or-split ()
-  (interactive)
-  (when (one-window-p)
-    (split-window-horizontally))
-  (other-window 1))
-(bind-key* "C-t" 'other-window-or-split)
+;; (defun other-window-or-split ()
+;;   (interactive)
+;;   (when (one-window-p)
+;;     (split-window-horizontally))
+;;   (other-window 1))
+;; (bind-key* "C-t" 'other-window-or-split)
 ;;(bind-key* "C-u C-t" 'delete-other-window)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
