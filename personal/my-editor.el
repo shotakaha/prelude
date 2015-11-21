7ff968a8906587606b176d15e0154c5d85b6dce3
Shota TAKAHASHI <shotakaha@gmail.com>
2015-11-21 22:10:48 +0900

    M : Add key-bind for yasnippet

--- personal/my-editor.el
+++ personal/my-editor.el
@@ -683,6 +683,10 @@
 ;;; yasnippet
 (use-package yasnippet
   :ensure t
+  :bind (:map yas-minor-mode-map
+              ("C-x i i" . yas-insert-snippet)
+              ("C-x i n" . yas-new-snippet)
+              ("C-x i v" . yas-visit-snippet-file))
   :config
   (yas-global-mode 1)
   (setq yas-prompt-functions '(yas-ido-prompt))
