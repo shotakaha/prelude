b676a6dd09305e9098d884bc5dde59b9a4703a87
Shota TAKAHASHI <shotakaha@gmail.com>
2015-11-25 10:10:34 +0900

    M : diminish yas-minor-mode

--- personal/my-editor.el
+++ personal/my-editor.el
@@ -683,6 +683,7 @@
 ;;; yasnippet
 (use-package yasnippet
   :ensure t
+  :diminish yas-minor-mode
   :bind (:map yas-minor-mode-map
               ("C-x i i" . yas-insert-snippet)
               ("C-x i n" . yas-new-snippet)
