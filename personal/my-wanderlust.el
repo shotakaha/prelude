20a1f556d363987b1429ab70fc347bd6b5bba0f0
Shota TAKAHASHI <shotakaha@gmail.com>
2016-10-09 18:42:38 +0900

    WanderLustの設定を追加

---
+++ personal/my-wanderlust.el
@@ -0,0 +1,18 @@
+;;; my-wanderlust.el --- My Wanderlust configuration
+
+;;; Commentary:
+
+;; wanderlust
+;; See : http://opamp.hatenablog.jp/entry/2015/01/07/210407
+;;
+;; Quickstart
+;; 1. Create ~/.wl
+;; 2. Create ~/.folder
+
+;;; Code:
+
+(use-package wanderlust)
+
+
+(provide 'my-wanderlust)
+;;; my-wanderlust.el ends here
